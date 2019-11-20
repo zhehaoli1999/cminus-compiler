c24082ba377b914b5adacb13f64c274e6c861b70
jasysj <jasysj@gmail>
2019-11-20 01:15:12 -0800

    add call array testcase

--- lab3-1/ysjRun.sh
+++ lab3-1/ysjRun.sh
@@ -1,4 +1,11 @@
 cd build
 cmake .. -DLLVM-DIR=/home/jasmine/lab/compiler/llvm-install/lib/cmake/llvm
 make -j
-./cminusc ../testcase/gcd.cminus -emit-llvm
+./cminusc ../testcase/array.cminus -emit-llvm
+# cd build
+# cmake .. -DLLVM_DIR=/home/jasmine/lab/compiler/llvm-install/lib/cmake/llvm
+# echo "========================================================="
+# read -p "cminus file name: " fname
+#  ./cminusc ../testcase/$fname.cminus -emit-llvm
+# echo "========================================================="
+# cat ../testcase/$fname.ll
